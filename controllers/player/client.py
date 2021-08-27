929592edc975109adc5e50ce22de27cca9903dff
heissonwillen <heisson70@gmail.com>
2021-08-27 18:53:50 -0300

    Add first version of client

--- controllers/player/client.py
+++ controllers/player/client.py
@@ -1,18 +1,9 @@
-import socket
+import sys
 
-import time
-
-PORT = 10002
-SERVER = "localhost"
-ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-
-if client == -1:
-    print("Cannot create socket")
-
-client.connect(ADDR)
-print(client.recv(2048).decode(FORMAT))
-
-time.sleep(10)
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    for arg_idx, current_arg in enumerate(args):
+        if current_arg[0] == '-':
+            if current_arg == '-v':
+                if arg_idx + 1 >= len(args):
+                    print("Missing value for verbosity")
