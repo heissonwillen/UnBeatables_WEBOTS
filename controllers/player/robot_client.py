e400c3136f94ada943abf3bd48979c3ff03cc022
heissonwillen <heisson70@gmail.com>
2021-08-28 02:14:04 -0300

    Update robot_client

--- controllers/player/robot_client.py
+++ controllers/player/robot_client.py
@@ -1,7 +1,14 @@
 import socket
+import time
 
 
 class RobotClient():
+    history_period = 5
+    # Adding some margin for other data than image
+    max_answer_size = 1920 * 1080 * 3 + 1000
+    max_attempts = 20
+    wait_time_sec = 1
+
     def __init__(self, host=None, port=None, verbosity=None):
         self.host = host
         self.port = port
@@ -12,13 +19,53 @@ class RobotClient():
         self.last_history_print = 0
 
     def connect_client(self):
-        print(self.host)
         server = socket.gethostbyname(self.host)
-        print(server)
-        pass
+        socket_fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def disconnect_client(self):
-        pass
+        if not socket_fd:
+            if self.verbosity > 0:
+                print("Cannot create socket")
+            return False
+
+        connected = False
+        for attempt in range(1, RobotClient.max_attempts+1):
+            try:
+                socket_fd.connect((server, self.port))
+                connected = True
+                break
+            except Exception:
+                print(
+                    f"Failed to connect to {self.host}:{self.port} (attempt {attempt} / {RobotClient.max_attempts})")
+                time.sleep(RobotClient.wait_time_sec)
+
+        if not connected:
+            if self.verbosity > 0:
+                print(
+                    "Failed to connect after {attempt} attempts. Giving up on connection")
+                socket_fd.close()
+            return False
+
+        answer = socket_fd.recv(8).decode("utf-8")
+        if self.verbosity >= 4:
+            print("Welcome message: {answer}")
+        if not "Welcome" in answer:
+            if self.verbosity > 0:
+                if "Refused" in answer:
+                    print(
+                        f"Connection to {self.host}:{self.port} refused: your IP address is not allowed in the game.json configuration file.")
+                else:
+                    print(f"Received unknown answer from server: {answer}")
+            socket_fd.close()
+            return False
+        if self.verbosity >= 2:
+            print(f"Connected to {self.host}:{self.port}")
+        return True
+
+    # def disconnect_client(self):
+    #     try:
+    #         socket_fd.close()
+    #     except Exception:
+    #         print("RobotClient is already disconnected")
 
     def send_request(self):
         pass
