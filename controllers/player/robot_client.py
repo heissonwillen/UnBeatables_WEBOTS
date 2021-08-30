9ebd77bdef6e4db9df8574cb72eab81cc9baeaf1
heissonwillen <heisson70@gmail.com>
2021-08-29 23:35:37 -0300

    Update robot client

--- controllers/player/robot_client.py
+++ controllers/player/robot_client.py
@@ -79,3 +79,15 @@ class RobotClient():
 
     def update_history(self):
         pass
+
+    def build_reques_message(self, path):
+        # f = open('a.txt', 'r')
+        # address_book = addressbook_pb2.AddressBook() # replace with your own message
+        # text_format.Parse(f.read(), address_book)
+        # f.close()
+
+        # f = open('b.txt', 'w')
+        # f.write(text_format.MessageToString(address_book))
+        # f.close()
+        with open(path, "r") as actuator_request_file:
+            pass
